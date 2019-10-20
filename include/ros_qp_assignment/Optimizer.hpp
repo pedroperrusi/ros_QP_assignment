25b1a189002c002de209089fa3742c20a8a02ddc
Pedro Perrusi <pedro.perrusi@gmail.com>
2019-10-20 14:52:55 +0000

    Created a print function to be called by the callback

--- include/ros_qp_assignment/Optimizer.hpp
+++ include/ros_qp_assignment/Optimizer.hpp
@@ -15,8 +15,8 @@
 #pragma once
 
 #include <ceres/ceres.h>
-#include "ros/ros.h"
 #include <std_msgs/Float64MultiArray.h>
+#include "ros/ros.h"
 
 #include "ros_qp_assignment/unconstrained_qp.hpp"
 
@@ -36,27 +36,34 @@ class Optimizer {
 
     ~Optimizer() {}
 
-    inline void run() { while(ros::ok()) ros::spin(); }
+    inline void run() {
+        while (ros::ok()) ros::spin();
+    }
 
     inline void paramCallback(
         const std_msgs::Float64MultiArray::ConstPtr& msg) {
         /* Deserialize coeffitients */
-        ros_qp::QuadraticCoeffs coeffs(msg->data.size(), msg->data.data() );
-	parameters_.resize(coeffs.order);
-	std::fill(parameters_.begin(), parameters_.end(), 1);
+        ros_qp::QuadraticCoeffs coeffs(msg->data.size(), msg->data.data());
+        parameters_.resize(coeffs.order);
+        std::fill(parameters_.begin(), parameters_.end(), 1);
         /** Initialize optimization routine */
         ceres::GradientProblem problem(new ros_qp::UnconstrainedQP(coeffs));
         ceres::Solve(options_, problem, parameters_.data(), &summary_);
-	if (summary_.termination_type != ceres::CONVERGENCE) {
-	  std::cout << "Solution did not converge" << std::endl;
-	}
-	std::cout << "Solution:" << std::endl;
-	auto solution = ros_qp::ConstMapType(parameters_.data(), coeffs.order, 1);
-	std::cout << solution << std::endl;
-	std::cout << "Total time:" << std::endl;
-	std::cout << summary_.total_time_in_seconds << std::endl;
+        printResults(coeffs.order);
     }
-   
+
+    inline void printResults(size_t order) {
+        if (summary_.termination_type != ceres::CONVERGENCE) {
+            std::cout << "Solution did not converge" << std::endl;
+        }
+        std::cout << "Solution:" << std::endl;
+        auto solution =
+            ros_qp::ConstMapType(parameters_.data(), order, 1);
+        std::cout << solution << std::endl;
+        std::cout << "Total time:" << std::endl;
+        std::cout << summary_.total_time_in_seconds << std::endl;
+    }
+
    private:
     ros::NodeHandle nh_;
     ros::Subscriber sub_;
--- src/optimizer_main.cpp
+++ src/optimizer_main.cpp
@@ -4,12 +4,12 @@
 #include "ros_qp_assignment/Optimizer.hpp"
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "optimizer_node");
-  // your optimizer class
-  Optimizer optimizer = Optimizer();
+    ros::init(argc, argv, "optimizer_node");
+    // your optimizer class
+    Optimizer optimizer = Optimizer();
 
-  // runs as long as roscore is running
-  optimizer.run();
+    // runs as long as roscore is running
+    optimizer.run();
 
-  return 0;
+    return 0;
 }
