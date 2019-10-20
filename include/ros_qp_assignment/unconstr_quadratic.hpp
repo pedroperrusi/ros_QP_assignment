b43ba8b83c37126a7b0639e7e6e2262169d72465
pedroperrusi <suruagyperrusi@etu.unistra.fr>
2019-10-20 15:19:13 +0200

    Refactored QuadraticCoefs structure

--- include/ros_qp_assignment/unconstr_quadratic.hpp
+++ include/ros_qp_assignment/unconstr_quadratic.hpp
@@ -1,7 +1,7 @@
 /**
  * @file unconstr_quadratic.h
  * @author Pedro Perrusi (pedro.perrusi@gmail.com)
- * @brief Uncontrained quadratic function definitions and structures
+ * @brief Uncontrained quadratic function structure containing  structures
  * @version 0.1
  * @date 2019-10-20
  *
@@ -10,6 +10,7 @@
  */
 #pragma once
 
+#include <vector>
 #include "ros_qp_assignment/types.hpp"
 
 namespace ros_qp {
@@ -27,23 +28,55 @@ namespace ros_qp {
  * @param num_elems: number of elements in a quadratic function parameter array
  * @return order of the quadratic function.
  */
-inline size_t inferOrder(size_t num_elems) {
+inline size_t inferOrder(const size_t num_elems) {
     return (-1 + sqrt(1 + 4 * num_elems)) / 2;
 }
 
 /**
  * @brief Quadratic function parameters
  *
+ * Receives a serialized input of size order*(order + 1).
+ * First the order of the system is infered by the number of elements.
+ * Then the data is deserialized into the Hessian matrix Q and the vector C.
+ *
  */
 struct QuadraticCoeffs {
-    size_t order;  // quadratic program order
-    double *data_ptr;  // pointer to the input coefficients
-    QuadraticCoeffs(size_t num_elems, double *input) : data_ptr(input) {
+    size_t order;                  // quadratic program order
+    std::vector<double> data_arr;  // pointer to the input coefficients
+    /**
+     * @brief Quadratic Coeffs constructor
+     *
+     * Infers the order of the quadratic function and creates a copy of its
+     * input parameters.
+     *
+     * Creating a copy was preffered in case the input data is
+     * not persistent.
+     *
+     * @param num_elems: number of elements in in_array
+     * @param in_array: data array containing the serialized parameters
+     */
+    QuadraticCoeffs(const size_t num_elems, const double *in_array) {
         order = inferOrder(num_elems);
+        data_arr.resize(num_elems);
+        data_arr.assign(in_array, in_array + num_elems);
     }
-    MatrixType getQ() const { return MapType(data_ptr, order, order); }
-    MatrixType getC() const {
-        return MapType(data_ptr + order * order, order, 1);
+
+    /**
+     * @brief Deserialize the Hessian matrix.
+     * 
+     * @return Matrix of shape order by order.
+     */
+    inline MatrixType getQ() const {
+        return ConstMapType(data_arr.data(), order, order);
+    }
+
+    /**
+     * @brief Deserialize the C vector.
+     * 
+     * @return Matrix of shape order by 1.
+     */
+    inline MatrixType getC() const {
+        return ConstMapType(data_arr.data() + order * order, order, 1);
     }
 };
 
@@ -52,7 +85,7 @@ struct QuadraticCoeffs {
  *
  * Receives Eigen Map types and returns an eigen matrix type.
  *
- * @param coeffs: QuadraticCoeffs definitions of order, Q and C.
+ * @param coeffs: structure containing order, Q and C.
  * @param x: Eigen type collumn vector of order coeffs.order.
  * @return MatrixType: Scalar interpreted as Matrix 1x1
  */
@@ -66,7 +99,7 @@ inline MatrixType quadraticCost(const QuadraticCoeffs coeffs, ConstMapType x) {
  *
  * Operator overload in accordance with Ceres input and outputs
  *
- * @param coeffs: QuadraticCoeffs definitions of order, Q and C.
+ * @param coeffs: structure containing order, Q and C.
  * @param parameters: input array of doubles of size coeffs.order
  * @param cost: output array of doubles of size 1
  */
@@ -83,7 +116,7 @@ inline void quadraticCost(const QuadraticCoeffs coeffs,
  * Inputs shoud be of dimension coeffs.order.
  * Output is a scalar.
  *
- * @param coeffs: QuadraticCoeffs definitions of order, Q and C.
+ * @param coeffs: structure containing order, Q and C.
  * @param x: Eigen type collumn vector of order coeffs.order.
  * @return MatrixType: Eigen type collumn vector of order coeffs.order.
  */
@@ -96,7 +129,7 @@ inline MatrixType quadraticGrad(const QuadraticCoeffs coeffs, ConstMapType x) {
  *
  * Operator overload in accordance with Ceres input and outputs
  *
- * @param coeffs: QuadraticCoeffs definitions of order, Q and C.
+ * @param coeffs: structure containing order, Q and C.
  * @param parameters: input array of doubles of size coeffs.order
  * @param grad: output array of doubles of size coeffs.order
  */
